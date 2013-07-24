d06ae260a877778e4d523f5b8937f7dcdab374d6
Kevin Johnson <kevin@coolkev.com>
2013-07-24 00:12:18 -0400

    Continued to set up the app. Models recieved a __unicode__() method. Admin was set up. Template directory was set up (doesn't look like its in the commit though, there should be a "templates" directory in the root directory)

---
+++ acapellaapp/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from acapellaapp.models import Group, Song, Track
+
+admin.site.register(Group)
+admin.site.register(Song)
+admin.site.register(Track)
+
--- acapellaapp/models.py
+++ acapellaapp/models.py
@@ -6,12 +6,19 @@ from django.contrib.auth.models import User
 class Group(models.Model):
     arranger = models.ForeignKey(User)
     name = models.CharField('group name',max_length=50)
+    def __unicode__(self):
+        return self.name;
+
 class Song(models.Model):
     group = models.ForeignKey(Group)
     title = models.CharField('song title', max_length=50)
     pub_date = models.DateTimeField('date published')
+    def __unicode__(self):
+        return self.title;
+        
 class Track(models.Model):
     song = models.ForeignKey(Song)
     name = models.CharField('track name' ,max_length=50)
     static_location = models.CharField('static location for this audio file', max_length=100)
-    
+    def __unicode__(self):
+        return self.name + " (" + self.song.title + ")";
