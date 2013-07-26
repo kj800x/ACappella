0e04b1902fa07b6a69017100db9efed8918fa9e5
Kevin Johnson <kevin@coolkev.com>
2013-07-26 03:14:38 -0400

    Began work on the css, I'm not sure if I like it.

--- acapellaapp/models.py
+++ acapellaapp/models.py
@@ -6,15 +6,21 @@ from django.contrib.auth.models import User
 class Group(models.Model):
     arranger = models.ForeignKey(User)
     name = models.CharField('group name',max_length=50)
+    short_code = models.CharField('unique, URL ready, shortcode', max_length=50)
     def __unicode__(self):
         return self.name;
+    class Meta:
+        ordering = ['name']
 
 class Song(models.Model):
     group = models.ForeignKey(Group)
     title = models.CharField('song title', max_length=50)
     pub_date = models.DateTimeField('date published')
+    short_code = models.CharField('unique, URL ready, shortcode', max_length=50)
     def __unicode__(self):
         return self.title;
+    class Meta:
+        ordering = ['-pub_date']
         
 class Track(models.Model):
     song = models.ForeignKey(Song)
