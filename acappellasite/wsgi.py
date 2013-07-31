a33f8ad3de58a2fd7b62077afb79f4b3327fc31b
Kevin Johnson <kevin@coolkev.com>
2013-07-30 23:52:15 -0400

    finished fixing the spelling of a cappella

--- acappellasite/wsgi.py
+++ acappellasite/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for acapellasite project.
+WSGI config for acappellasite project.
 
 This module contains the WSGI application used by Django's development server
 and any production WSGI deployments. It should expose a module-level variable
